636e7aa1494eac708c6c9ee24846f4a662c49b5f
m0ng00se <m0ng00se3133@yandex.com>
2018-01-01 02:12:18 -0800

    Adding outline for merge sort

---
+++ Chapter01/MergeSort.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSort extends Sort {
+
+    public <T extends Comparable<T>> void sort(List<T> m) {
+
+    }
+
+    //! No implementation
+    public <T extends Comparable<T>> List<T> sortWithCopy(List<T> m) {
+	return null;
+    }
+
+    //! Unit tests
+    public static void main(String[] args) {
+
+    }
+}
